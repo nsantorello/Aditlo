f5d1e711bfcaf15fdfe3f9da0ecc26831fb2cb61
Noah Santorello <nsantorello@gmail.com>
2011-02-12 05:20:25 -0500

    Added empty Xcode project.

---
+++ Classes/AditloAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AditloAppDelegate.h
+//  Aditlo
+//
+//  Created by Noah Santorello on 2/12/11.
+//  Copyright 2011 Infusion Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AditloAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Aditlo
+//
+//  Created by Noah Santorello on 2/12/11.
+//  Copyright 2011 Infusion Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
