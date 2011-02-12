e252fb50c538193ae6dec82f25dcf687f7225fd8
Noah Santorello <nsantorello@gmail.com>
2011-02-12 17:26:26 -0500

    Removed the Infusion Development copyright references, just in case...

--- Classes/MainViewController.m
+++ Classes/MainViewController.m
@@ -3,7 +3,7 @@
 //  Aditlo
 //
 //  Created by Noah Santorello on 2/12/11.
-//  Copyright 2011 Infusion Development. All rights reserved.
+//  Copyright 2011 Noah Santorello. All rights reserved.
 //
 
 #import "MainViewController.h"
