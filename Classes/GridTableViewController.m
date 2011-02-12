b1aea0c2c440fb6d1aa2025647db67cf16f2a0d9
Noah Santorello <nsantorello@gmail.com>
2011-02-12 18:12:47 -0500

    Going to roll with the 3x3 grid and hope that it works better on the device than on the simulator.

--- Classes/GridTableViewController.m
+++ Classes/GridTableViewController.m
@@ -22,7 +22,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     // Return the number of rows in the section.
-    return 5;
+    return 10;
 }
 
 
