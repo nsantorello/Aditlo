4c3bdaff5017de5432a0ccee3b9ded489315d620
Noah Santorello <nsantorello@gmail.com>
2011-02-13 13:59:25 -0500

    Updated route on Server to use a QueryController.

--- Adilla-Server/Adilla/config/routes.rb
+++ Adilla-Server/Adilla/config/routes.rb
@@ -3,7 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :adils
   
   # Routes for mobile access
-  map.m_today '/today', :controller => 'query', :action => 'today'
+  map.m_today '/today.:format', :controller => 'query', :action => 'today'
   # map.m_post '/post', :controller => 'adils', :action => 'post'
   # map.m_intr '/intr', :controller => 'adils', :action => 'intr'
   # map.m_feed '/feed', :controller => 'adils', :action => 'feed'
