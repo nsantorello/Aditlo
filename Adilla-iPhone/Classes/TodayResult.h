aca1207098dc9fab7f57b5cc6ce2582a087c3431
Noah Santorello <nsantorello@gmail.com>
2011-02-13 09:44:32 -0500

    Added JSON library at https://github.com/stig/json-framework/downloads to the project.

---
+++ Adilla-iPhone/Classes/TodayResult.h
@@ -0,0 +1,16 @@
+//
+//  TodayResult.h
+//  Aditlo
+//
+//  Created by Noah Santorello on 2/13/11.
+//  Copyright 2011 Noah Santorello. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TodayResult : NSObject {
+
+}
+
+@end
---
+++ Adilla-iPhone/Classes/TodayResult.m
@@ -0,0 +1,14 @@
+//
+//  TodayResult.m
+//  Aditlo
+//
+//  Created by Noah Santorello on 2/13/11.
+//  Copyright 2011 Noah Santorello. All rights reserved.
+//
+
+#import "TodayResult.h"
+
+
+@implementation TodayResult
+
+@end
