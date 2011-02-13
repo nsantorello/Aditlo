ed2e902167f8884b02216d49d0db44ba96d10bc3
Noah Santorello <nsantorello@gmail.com>
2011-02-13 01:02:44 -0500

    Added (initial, empty) Android project to repo.

---
+++ Adilla-Android/src/com/nsantorello/adilla/Adilla.java
@@ -0,0 +1,13 @@
+package com.nsantorello.adilla;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Adilla extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
