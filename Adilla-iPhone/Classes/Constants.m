1068d4796e0d91f28f6cee16daa6e9813538d91e
Noah Santorello <nsantorello@gmail.com>
2011-02-13 14:13:04 -0500

    Going to begin working on async image loading in the table.

--- Adilla-iPhone/Classes/Constants.m
+++ Adilla-iPhone/Classes/Constants.m
@@ -14,7 +14,7 @@ NSString* const AdillaUrl_Server = @"http://adil.la/";
 NSString* const AdillaUrl_Today = @"today.json";
 
 #ifdef DEV_CONSTANTS
-NSString* const AwsDistUrl_Server = @"";//http://dl.adil.la.s3.amazonaws.com/";
+NSString* const AwsDistUrl_Server = @"";//@"http://dl-dev.adil.la.s3.amazonaws.com/";
 NSString* const AwsDistUrl_Thumbs = @"";//@"thumbs/";
 NSString* const AwsDistUrl_Adils = @"";//@"adils/";
 #else
